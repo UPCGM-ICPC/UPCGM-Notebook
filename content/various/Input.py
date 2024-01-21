8b648df0afe0b3df044e8b7c5dc5c72a2d82ec5e
Yago Iglesias <yago.iglesias.vazquez@gmail.com>
2024-01-21 22:24:36 +0100

    refactor: swap examples

--- content/various/Input.py
+++ content/various/Input.py
@@ -7,5 +7,5 @@
 a = int(input())  # Read an int
 raw = input()  # Read a string
 s = "sep :) "; raw = raw.split(s)  # Split the string by the separator s (default is space)
-ints = [int(x) for x in raw]  # Convert the splitted string to a list of ints
 a, b, c = int(raw[0]), int(raw[1]), int(raw[2])  # Read 3 ints
+ints = [int(x) for x in raw]  # Convert the splitted string to a list of ints
