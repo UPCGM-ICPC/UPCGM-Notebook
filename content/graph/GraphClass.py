da43244933e24de93b1c50cef9de15a229d95e74
Yago Iglesias <yagoiglesias7@gmail.com>
2024-01-21 22:25:58 +0100

    chore: remove unnecessary white space (#9)

--- content/graph/GraphClass.py
+++ content/graph/GraphClass.py
@@ -3,8 +3,6 @@
 * Description: Graph class. Basic structure for graphs.
 """
 from collections import defaultdict
-
-
 class Graph:
     def __init__(self, v, egdes=None):
         self.v = v
@@ -12,21 +10,17 @@ class Graph:
         if egdes:
             for u, v, w in egdes:
                 self.add_edge(u, v, w)
-
     def add_edge(self, u, v, w):
         self.edges[u][v] = w
-
     def get_neighbors(self, u):
         neighbors = []
         for v in self.edges[u].keys():
             neighbors.append(v)
         return neighbors
-
     def get_edge(self, u, v):
         if v in self.edges[u]:
             return self.edges[u][v]
         return None
-
     def get_edges(self):
         edges = []
         for u in self.edges.keys():
