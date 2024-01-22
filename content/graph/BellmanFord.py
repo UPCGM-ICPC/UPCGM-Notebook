1a6fc73a93cdd617b72a201d7a9a217f7a154d5b
Yago Iglesias <yagoiglesias7@gmail.com>
2024-01-22 15:35:00 +0100

    feat: Add deploy notebook action (#11)
    * feat: Add deploy notebook action
    
    * fix: typo

---
+++ content/graph/BellmanFord.py
@@ -0,0 +1,23 @@
+"""
+ * Author: Unknown
+ * Source: https://www.geeksforgeeks.org/bellman-ford-algorithm-dp-23/ 
+ * Description: Computes shortest paths from $start$. Allows negative-weighted cycles
+ * Time: $O(VE)$
+"""
+
+
+def bellmanFord(graph, start_vertex):
+    distance = [float("inf") for _ in range(graph.v)]
+    distance[start_vertex] = 0
+
+    for _ in range(graph.v - 1):
+        for u, v, w in graph.get_weighted_edges():
+            if distance[u] != float("inf") and distance[u] + w < distance[v]:
+                distance[v] = distance[u] + w
+
+    for _ in range(graph.v - 1):
+        for u, v, w in graph.get_weighted_edges():
+            if distance[u] != float("inf") and distance[u] + w < distance[v]:
+                distance[v] = -float("inf")
+
+    return distance
