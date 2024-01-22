0ec6429a3ef0d49b06432aac403168d6486f052f
gdudilli <gabin.dudillieu@etu.u-paris.fr>
2024-01-22 10:58:11 +0100

    chore: remove useless whitespaces

--- content/combinatorial/PermAndCombination.py
+++ content/combinatorial/PermAndCombination.py
@@ -6,23 +6,17 @@
          O(n^r)  for permutations with repetitions
          O(n!/(r!*((n-r))!)) for combinations
 """
-
 from itertools import permutations, product, combinations
- 
 def generate_permutations(lst, r=None):
     return list(permutations(lst, r))
-
 def generate_permutations_with_repetition(lst, r=None):
     if r == None:
         r = len(lst)
     return list(product(lst, repeat=r))
-
 def generate_int_permutation(n, r=None):
     lst = [(j + 1) for j in range(n)]
     return list(generate_permutations(lst, r))
-
 def generate_combination(lst, r=None):
     if r == None:
         r = len(lst)
     return list(combinations(lst, r))
-
