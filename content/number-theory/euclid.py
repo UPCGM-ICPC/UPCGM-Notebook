321df4d662cfd4f1e2b6e511f8436051dfae6c9e
gdudilli <gabin.dudillieu@etu.u-paris.fr>
2024-01-26 10:24:19 +0100

    feat: add euclid algorithm
    to squash
    
    to squash
    
    to squash
    
    to squash
    
    to squash

---
+++ content/number-theory/euclid.py
@@ -0,0 +1,20 @@
+# Author: Gabin Dudillieu
+# Date: 2024-01-26
+# Source: https://www.rookieslab.com/posts/extended-euclid-algorithm-to-find-gcd-bezouts-coefficients-python-cpp-code
+# Description: Finds gcd, and finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
+# you just need gcd, use the built in \texttt{\_\_gcd} instead.
+# If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
+from math import gcd
+def get_gcd(a, b):
+    return gcd(a, b)
+def extended_euclid_gcd(a, b):
+    # Referring to the equation ax + by = gcd(a, b), result[0] is gcd(a, b), result[1] is x, result[2] is y 
+    s = 0; old_s = 1
+    t = 1; old_t = 0
+    r = b; old_r = a
+    while r != 0:
+        quotient = old_r // r 
+        old_r, r = r, old_r - quotient*r
+        old_s, s = s, old_s - quotient*s
+        old_t, t = t, old_t - quotient*t
+    return [old_r, old_s, old_t]
