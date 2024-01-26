5b966fd0ca401679a691689b5064dc7af78c307c
gdudilli <gabin.dudillieu@etu.u-paris.fr>
2024-01-26 11:19:22 +0100

    fix: correct format and unused comment

--- content/number-theory/FastEratosthenes.py
+++ content/number-theory/FastEratosthenes.py
@@ -1,13 +1,8 @@
 """
- Author: Gabin Dudillieu
- Date: 2024-01-26
- Description: Prime sieve for generating all primes smaller than LIM.
- Time: LIM=1e9 $\approx$ 1.5s
- Status: Stress-tested
- Details: Despite its n log log n complexity, segmented sieve is still faster
- than other options, including bitset sieves and linear sieves. This is
- primarily due to its low memory usage, which reduces cache misses. This
- implementation skips even numbers.
+* Author: Gabin Dudillieu
+* Date: 2024-01-26
+* Description: Prime sieve for generating all primes smaller than LIM.
+* Time: LIM=1e9 $\approx$ 1.5s
 """
 def SieveOfEratosthenes(num):
     result = []
